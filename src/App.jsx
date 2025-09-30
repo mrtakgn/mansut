232ab13c11a7258f493a60e1faa6d127d4be1b81
mrtakgn <mrtakgn1943@gmail.com>
2025-09-30 03:08:45 +0300

    sonuncu

--- src/App.jsx
+++ src/App.jsx
@@ -149,14 +149,14 @@ function Home() {
   );
 }
 
-function Hakkimizda() {
+function Hakkimizda()  {
   return (
     <div className="page about-page">
       <div className="about-header">
         <img src="https://lh3.googleusercontent.com/gps-cs/AIky0YUDC-cje0H6zsS0ClYR-_ZrmpYBx_5Qxp1qW_mohqWBZMzYoaQ98yZ66UJcYFGySOlO6ObsdnwAcnsCdtMHTXM0aRXhh-fsusLD__FEMW9Gdf5dqHdyc7HGxbcaCGb-UEeGoh0E=w750-h401-p-k-no" alt="Mansüt Gıda Fabrika" className="about-img" />
         <div className="about-content">
           <h1>Hakkımızda</h1>
-          <p>Mansüt Gıda, 1998 yılından bu yana süt ve süt ürünleri sektöründe faaliyet göstermektedir. Amacımız, en taze ve sağlıklı ürünleri, en yüksek kalite standartlarında sizlere sunmaktır. Doğaya ve insana saygılı üretim anlayışımızla, sürdürülebilir tarımı ve yerel çiftçileri destekliyoruz.</p>
+          <p>Mansüt Gıda, 1999 yılından bu yana süt ve süt ürünleri sektöründe faaliyet göstermektedir. Amacımız, en taze ve sağlıklı ürünleri, en yüksek kalite standartlarında sizlere sunmaktır. Doğaya ve insana saygılı üretim anlayışımızla, sürdürülebilir tarımı ve yerel çiftçileri destekliyoruz.</p>
           <ul>
             <li>25+ yıllık tecrübe</li>
             <li>Modern ve hijyenik üretim tesisleri</li>
